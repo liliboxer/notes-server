24928370e1e97ddc9c3f94737559594a4e8ccee7
Lili Boxer <ninaliboxer@gmail.com>
2019-08-19 10:01:11 -0700

    post route working

--- test/app.test.js
+++ test/app.test.js
@@ -5,16 +5,26 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 
-describe('app routes', () => {
+describe('notes routes', () => {
   beforeAll(() => {
     connect();
-  });
-
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
   });
 
   afterAll(() => {
     return mongoose.connection.close();
   });
-});
+
+  it('can post note', () => {
+    return request(app)
+      .post('/api/v1/notes')
+      .send({ title: 'first note!', body: 'this is a note' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          title: expect.any(String),
+          body: expect.any(String),
+        });
+      });
+  });
+})
+;
