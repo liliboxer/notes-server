dcaf9664e20319b97a0af8bd362b55364c2ce102
Lili Boxer <ninaliboxer@gmail.com>
2019-08-19 11:49:04 -0700

    get all notes route written and passing

--- test/app.test.js
+++ test/app.test.js
@@ -5,6 +5,8 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 
+const Note = require('../lib/Models/Note');
+
 describe('notes routes', () => {
   beforeAll(() => {
     connect();
@@ -21,8 +23,28 @@ describe('notes routes', () => {
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          title: expect.any(String),
-          body: expect.any(String),
+          title: 'first note!',
+          body: 'this is a note',
+        });
+      });
+  });
+
+  it('GET all notes', async() => {
+    const notes = await Note.create([
+      { title: 'note1', body: 'body1' },
+      { title: 'note2', body: 'body2' },
+      { title: 'note3', body: 'body3' }
+    ]);
+    return request(app)
+      .get('/api/v1/notes')
+      .then(res => {
+        const notesJSON = JSON.parse(JSON.stringify(notes));
+        notesJSON.forEach(note => {
+          expect(res.body).toContainEqual({ 
+            title: note.title, 
+            body: note.body,
+            _id: note._id.toString()
+          });
         });
       });
   });
